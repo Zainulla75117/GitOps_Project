28b089eea880309470ce0dbe0eff0808784f68b4
Zainulla75117 <pzainullabideen@gmail.com>
2026-08-09 00:00:48 +0530

    fix: iam oidc trust policy sub claim casing

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -41,7 +41,10 @@ resource "aws_iam_role" "github_actions" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:${var.github_repo}:*",
+              "repo:${lower(var.github_repo)}:*"
+            ]
           }
         }
       }
